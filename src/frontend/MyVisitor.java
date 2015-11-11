ae9381ce1cbebcaaf8a90b7d8437a67097bde1e0
De Lee <dsl114@corona19.doc.ic.ac.uk>
2015-11-11 11:20:44 +0000

    [dsl114][cyl113] Clean up and document code.

--- src/frontend/MyVisitor.java
+++ src/frontend/MyVisitor.java
@@ -1,94 +1,52 @@
 package frontend;
 
-import org.antlr.v4.runtime.tree.ParseTree;
-
 import antlr.WACCParser;
 import antlr.WACCParserBaseVisitor;
 
 public class MyVisitor extends WACCParserBaseVisitor<Void> {
 
-    private int syntaxErrorCount = 0;
-    private boolean inFunction = false;
+	private int syntaxErrorCount = 0;
 
-    private Void syntaxError() {
-        // Need to pass alot more information here to throw a proper error message
-        syntaxErrorCount += 1;
-        return null;
-    }
+	private Void syntaxError() {
+		// TODO: (Print out the error messages. Refer to referred compiler for
+		// the messages)
+		syntaxErrorCount += 1;
 
-    private Void returnCheck(String statement) {
-        final int returnLength = 6;
-        if (statement.length() > returnLength) {
-            statement = statement.substring(0, returnLength);
-        }
-        if (!statement.equals("return")) {
-            syntaxError();
-        }
-        return null;
+		return null;
+	}
 
-    }
+	public Void visitProgram(WACCParser.ProgramContext ctx) {
+		return visitChildren(ctx);
+	}
 
-    public Void visitProgram(WACCParser.ProgramContext ctx) {
-        return visitChildren(ctx);
-    }
+	public Void visitExpr(WACCParser.ExprContext ctx) {
+		return null;
+	}
 
-    public Void visitExpr(WACCParser.ExprContext ctx) {
-        return null;
-    }
+	public Void visitStat(WACCParser.StatContext ctx) {
+		// System.out.println("-Statement");
+		return visitChildren(ctx);
+	}
 
-    public Void visitStat(WACCParser.StatContext ctx) {
-        // System.out.println("-Statement");
-        return visitChildren(ctx);
-    }
+	public Void visitFunc(WACCParser.FuncContext ctx) {
+		System.out.println("I found a function definition!");
+		// TODO
 
-    public Void visitFunc(WACCParser.FuncContext ctx) {
-        System.out.println("I found a function definition!");
-        inFunction = true;
-//        int childCount = ctx.getChildCount();
-        // for (int i = 0; i < childcount; i++) {
-        // System.out.println(ctx.getChild(i));
-        // }
+		return null;
+	}
 
-        // Visit the statements
-        int statementCount = ctx.stat().getChildCount();
-        System.out.println(ctx.stat());
-        System.out.println("depth: " + ctx.depth());
-        System.out.println(statementCount);
+	// Visiting literals
+	public Void visitIntegerliteral(WACCParser.IntegerliteralContext ctx) {
+		try {
+			@SuppressWarnings("unused")
+			Integer value = Integer.parseInt(ctx.getChild(0).getText());
+		} catch (NumberFormatException e) {
+			syntaxError();
+		}
+		return null;
+	}
 
-        if (statementCount == 2) {
-            returnCheck(ctx.stat().getChild(0).toString());
-        } else {
-            for (int i = 0; i < statementCount; i++) {
-                ParseTree child = ctx.stat().getChild(i);
-                System.out.println(child.getClass());
-                System.out.println(child.getParent().getClass());
-                visit(child);
-                if (i == statementCount - 1) {
-                    if (child.getClass() != WACCParser.ReturnstatementContext.class) {
-                        syntaxError();
-                    }
-                }
-            }
-        }
-        
-        inFunction = false;
-        return null;
-    }
-
-    // Visiting literals
-    public Void visitIntegerliteral(
-                    WACCParser.IntegerliteralContext ctx) {
-        try {
-            @SuppressWarnings("unused")
-            Integer value = Integer.parseInt(ctx.getChild(0)
-                            .getText());
-        } catch (NumberFormatException e) {
-            syntaxError();
-        }
-        return null;
-    }
-
-    public int getSyntaxErrorCount() {
-        return syntaxErrorCount;
-    }
+	public int getSyntaxErrorCount() {
+		return syntaxErrorCount;
+	}
 }
--- src/frontend/WACCErrorListener.java
+++ src/frontend/WACCErrorListener.java
@@ -13,43 +13,30 @@ import org.antlr.v4.runtime.misc.Nullable;
 
 public class WACCErrorListener implements ANTLRErrorListener {
 
-    @Override
-    public void reportAmbiguity(@NotNull Parser arg0,
-                    @NotNull DFA arg1, int arg2, int arg3,
-                    boolean arg4, @Nullable BitSet arg5,
-                    @NotNull ATNConfigSet arg6) {
-        // TODO Auto-generated method stub
+	@Override
+	public void reportAmbiguity(@NotNull Parser arg0, @NotNull DFA arg1,
+			int arg2, int arg3, boolean arg4, @Nullable BitSet arg5,
+			@NotNull ATNConfigSet arg6) {
+	}
 
-    }
+	@Override
+	public void reportAttemptingFullContext(@NotNull Parser arg0,
+			@NotNull DFA arg1, int arg2, int arg3, @Nullable BitSet arg4,
+			@NotNull ATNConfigSet arg5) {
+	}
 
-    @Override
-    public void reportAttemptingFullContext(@NotNull Parser arg0,
-                    @NotNull DFA arg1, int arg2, int arg3,
-                    @Nullable BitSet arg4, @NotNull ATNConfigSet arg5) {
-        // TODO Auto-generated method stub
+	@Override
+	public void reportContextSensitivity(@NotNull Parser arg0,
+			@NotNull DFA arg1, int arg2, int arg3, int arg4,
+			@NotNull ATNConfigSet arg5) {
+	}
 
-    }
-
-    @Override
-    public void reportContextSensitivity(@NotNull Parser arg0,
-                    @NotNull DFA arg1, int arg2, int arg3, int arg4,
-                    @NotNull ATNConfigSet arg5) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void syntaxError(@NotNull Recognizer<?, ?> arg0,
-                    @Nullable Object arg1, int line, int col,
-                    @NotNull String errorMessage,
-                    @Nullable RecognitionException arg5) {
-        // System.out.println(arg0.toString());
-        // System.out.println(arg1.toString());
-        System.err.println("Syntactic error at " + line + ":" + col
-                        + " -- " + errorMessage);
-        // System.out.println(arg2 + " " + arg3); // Position of the error
-        // System.out.println(arg4.toString()); // Some default error message
-        // System.out.println(arg5.toString()); // Type of exception
-    }
+	@Override
+	public void syntaxError(@NotNull Recognizer<?, ?> arg0,
+			@Nullable Object arg1, int line, int col,
+			@NotNull String errorMessage, @Nullable RecognitionException arg5) {
+		System.err.println("Syntactic error at " + line + ":" + col + " -- "
+				+ errorMessage);
+	}
 
 }
